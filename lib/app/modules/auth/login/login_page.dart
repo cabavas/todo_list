5cac5ac6988dd436352b17653f4492561eea7ea2
Cabavas <cabavas@gmail.com>
2023-03-01 18:45:05 -0300

    First commit

---
+++ lib/app/modules/auth/login/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: IntrinsicHeight(
+                child: Column(),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
