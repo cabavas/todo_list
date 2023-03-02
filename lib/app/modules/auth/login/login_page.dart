42174c9d23c5d4476fa6814fdfa282ba562d1a23
Cabavas <cabavas@gmail.com>
2023-03-02 17:48:04 -0300

    Login Layout

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:todo_list/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -15,7 +18,87 @@ class LoginPage extends StatelessWidget {
                 minWidth: constraints.maxWidth,
               ),
               child: IntrinsicHeight(
-                child: Column(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 10),
+                    const TodoListLogo(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      child: Form(
+                        child: Column(
+                          children: [
+                            TextFormField(),
+                            const SizedBox(height: 20),
+                            TextFormField(),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Esqueceu sua senha?'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                  ),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(10.0),
+                                    child: Text('Login'),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xfff0f3f7),
+                          border: Border(
+                            top: BorderSide(
+                              width: 2,
+                              color: Colors.grey.withAlpha(50),
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 30),
+                            SignInButton(
+                              Buttons.Google,
+                              text: 'Continue com o Google',
+                              padding: const EdgeInsets.all(5),
+                              shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              onPressed: () {},
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Não tem conta?'),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Cadastre-se'),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
